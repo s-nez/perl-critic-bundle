eb8c0333c2dee394556616471f8af485502c1a85
Szymon Nieznański <s.nez@member.fsf.org>
2019-08-09 11:23:40 +0300

    Initial commit

---
+++ lib/Perl/Critic/PolicyBundle/SNEZ.pm
@@ -0,0 +1,29 @@
+package Perl::Critic::PolicyBundle::SNEZ;
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+1;
+__END__
+=pod
+
+=head1 NAME
+
+Perl::Critic::PolicyBundle::SNEZ - extra Perl::Critic policies
+
+=head1 DESCRIPTION
+
+This is a set of policies I deemed useful but were not present in the core
+Perl::Critic (as far as I know).
+
+=head1 AUTHOR
+
+Szymon Nieznański <snez@cpan.org>
+
+=head1 COPYRIGHT
+
+This program is Free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
